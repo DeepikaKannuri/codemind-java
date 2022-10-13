cc90e34219a736a703907c46a1ae3d2eff4a0e59
DeepikaKannuri <104634154+DeepikaKannuri@users.noreply.github.com>
2022-10-13 23:24:49 +0530

    import java.util.*; class Area {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int a=in.nextInt();         int b=in.nextInt();         int c=in.nextInt();         int ans=2*a*b*c*512;         System.out.print(ans);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Area {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=in.nextInt();
+        int ans=2*a*b*c*512;
+        System.out.print(ans);
+    }
+}
